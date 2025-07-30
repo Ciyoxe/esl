b8df3271f4d0883ffda95b47dd143ab436c7d3bc
ciyoxe <ciyoxe@gmail.com>
2025-07-30 16:54:20 +0700

    Op settings mapping

--- src/ast/expr_builder.rs
+++ src/ast/expr_builder.rs
@@ -1,3 +1,34 @@
 use crate::{ast::{build::build_expression, Operation, AstNode}, Rule};
 use pest::iterators::{Pair, Pairs};
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Assoc {
+    Left,
+    Right,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct OpInfo {
+    pub precedence: u8,
+    pub assoc: Assoc,
+}
+
+impl OpInfo {
+    pub const fn new(precedence: u8, assoc: Assoc) -> Self {
+        Self { precedence, assoc }
+    }
+
+    pub fn of(rule: &Rule) -> Option<Self> {
+        match rule {
+            Rule::op_add => Some(Self::new(0, Assoc::Left)),
+            Rule::op_sub => Some(Self::new(0, Assoc::Left)),
+            Rule::op_mul => Some(Self::new(0, Assoc::Left)),
+            Rule::op_div => Some(Self::new(0, Assoc::Left)),
+            Rule::op_neg => Some(Self::new(0, Assoc::Left)),
+            Rule::op_not => Some(Self::new(0, Assoc::Left)),
+            Rule::op_cat => Some(Self::new(0, Assoc::Left)),
+
+            _ => None,
+        }
+    }
+}
